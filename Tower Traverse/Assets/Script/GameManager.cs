0b85d34cd5a6be29b37e750ca2b0ac197170e17a
Andrew <andrewclark030@gmail.com>
2023-04-27 23:12:22 -0400

    press R to go back to main menu

--- Tower Traverse/Assets/Script/GameManager.cs
+++ Tower Traverse/Assets/Script/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -20,4 +21,9 @@ public class GameManager : MonoBehaviour
     #endregion singleton
 
     public string currentLevel;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R)) SceneManager.LoadScene("StartScene");
+    }
 }
